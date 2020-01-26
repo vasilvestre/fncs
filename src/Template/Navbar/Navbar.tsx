af00874adfaf20316a160a95ab91e5aa9bfd1ae9
Valentin Silvestre <vasilvestre@laposte.net>
2020-01-26 22:15:11 +0100

    WORKS

--- src/Template/Navbar/Navbar.tsx
+++ src/Template/Navbar/Navbar.tsx
@@ -1,89 +1,97 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import Switch from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormGroup from '@material-ui/core/FormGroup';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import AccountCircle from '@material-ui/icons/AccountCircle'
+import MenuItem from '@material-ui/core/MenuItem'
+import Menu from '@material-ui/core/Menu'
 import NavbarStyle from './NavbarStyle'
 import { Button } from '@material-ui/core'
 import LoginModal from '../Login/LoginModal'
-import DialogTitle from '@material-ui/core/DialogTitle'
-import { BrowserRouter as Router } from 'react-router-dom'
-import Dialog from '@material-ui/core/Dialog'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
 
 export default function Navbar() {
     const classes = NavbarStyle.useStyles()
-    const [auth, setAuth] = React.useState(false);
-    const [open, setOpen] = React.useState(false);
-    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [auth, setAuth] = React.useState(false)
+    const [open, setOpen] = React.useState(false)
+    const [openLogin, setOpenLogin] = React.useState(false)
+    const [anchorEl, setAnchorEl] = React.useState(null)
+
+    const handleClickOpenLogin = () => {
+        setOpenLogin(true)
+    }
+
+    const handleCloseOpenLogin = () => {
+        setOpenLogin(false)
+    }
+
+    const handleClose = () => {
+        setOpen(false)
+    }
 
     const handleClickOpenModal = () => {
-        setOpen(true);
-    };
-
-    const handleClose = (value: string) => {
-        setOpen(false);
-    };
+        setOpen(true)
+    }
 
     const handleMenu = (event: any) => {
-        setAnchorEl(event.currentTarget);
-    };
+        setAnchorEl(event.currentTarget)
+    }
 
     const handleMenuClose = () => {
-        setAnchorEl(null);
-    };
+        setAnchorEl(null)
+    }
     return (
         <div className={classes.root}>
             <AppBar position="static">
 
                 <Router>
-                <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        FNCS
-                    </Typography>
-                    <Button color="inherit" onClick={handleClickOpenModal}>Login</Button>
-                    <LoginModal open={open} onClose={handleClose} />
-                </Toolbar>
-                {auth && (
-                    <div>
-                        <IconButton
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleMenu}
-                            color="inherit"
-                        >
-                            <AccountCircle />
+                    <Toolbar>
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                            <MenuIcon/>
                         </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorEl}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={open}
-                            onClose={handleMenuClose}
-                        >
-                            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-                            <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-                        </Menu>
-                    </div>
-                )}
+                        <Typography variant="h6" className={classes.title}>
+                            FNCS
+                        </Typography>
+                        {!auth && (
+                            <Button color="inherit" onClick={handleClickOpenLogin}>
+                                <Link style={{ textDecoration: 'none', color: 'white' }} to="/sign_in">Login</Link>
+                            </Button>
+                        )}
+                        <LoginModal open={openLogin} onClose={handleCloseOpenLogin}/>
+                        {auth && (
+                            <div>
+                                <IconButton
+                                    aria-label="account of current user"
+                                    aria-controls="menu-appbar"
+                                    aria-haspopup="true"
+                                    onClick={handleMenu}
+                                    color="inherit"
+                                >
+                                    <AccountCircle/>
+                                </IconButton>
+                                <Menu
+                                    id="menu-appbar"
+                                    anchorEl={anchorEl}
+                                    anchorOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    open={open}
+                                    onClose={handleMenuClose}
+                                >
+                                    <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+                                    <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+                                </Menu>
+                            </div>
+                        )}
+                    </Toolbar>
                 </Router>
             </AppBar>
         </div>
